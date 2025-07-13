724df8b2dc347abe8c1e7ea6f1062c9c97c311e0
edoardo1974 <edoardo.mozzato@gmail.com>
2025-07-13 17:59:23 +0100

    Add function to count Twitter links in text
    Introduced the imprimirLinktwitter function to count and display the number of links that open Twitter. Updated the HTML to wire the new function to the corresponding button and added a div to display the result.

--- js/analisetexto.js
+++ js/analisetexto.js
@@ -37,12 +37,32 @@ function imprimirpenúltimolink(){
     encontrarLinksDeTexto();
     const linkdotesto = document.getElementById("penultimolinkdotesto");
     linkdotesto.innerHTML = "";
+    const p = document.createElement("p");
+    p.textContent = links[links.length - 2]; // penúltimo link
+    linkdotesto.appendChild(p);
+    } 
 
+function imprimirLinktwitter(){
+    encontrarLinksDeTexto();
+    const linkdotesto = document.getElementById("twitterlinkdotesto");
+    linkdotesto.innerHTML = "";
+    let count = 0;
     for (let i = 0; i < links.length; i++) {
-        const p = document.createElement("p");
-        p.textContent = links[i];
-        linkdotesto.appendChild(p);
+        if( links[i].includes("file:///C:/Users/hp%206470/Documents/Front%20end/M3P13/html/www.twitter.com")) {
+            console.log(links[i]);
+            const p = document.createElement("p");
+            p.textContent = links[i];
+            linkdotesto.appendChild(p);
+            count++;
+        }
     }
 
+    const p = document.createElement("p");
+    p.textContent = "Número de links que abrem Twitter: " + count;
+    linkdotesto.appendChild(p);
 
     } 
+
+function imprimirLinksTerceiroParagrafo() {
+
+    }
